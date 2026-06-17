834c8e9b934fb98312a97a73bb86ed0241aae65b
Yugo Alfrzi <yugoalfrzi@gmail.com>
2026-06-17 22:26:55 +0700

    penyesuaian tampilan manage-account

--- resources/views/superadmin/manage-accounts.blade.php
+++ resources/views/superadmin/manage-accounts.blade.php
@@ -5,19 +5,6 @@
 
 @section('content')
 
-@if(session('success'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-    <i class="bi bi-check-circle me-2"></i>{{ session('success') }}
-    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-</div>
-@endif
-@if(session('error'))
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    <i class="bi bi-exclamation-circle me-2"></i>{{ session('error') }}
-    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-</div>
-@endif
-
 {{-- Stats --}}
 <div class="row g-3 mb-4">
     <div class="col-6 col-md-3">
